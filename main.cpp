caa6a89e90813c3adedd1dd55e72584842c9dced
Sean Theisen <seanjtheisen@gmail.com>
2018-09-26 14:07:16 -0400

    Commented and cleaned

--- main.cpp
+++ main.cpp
@@ -11,13 +11,12 @@
 
 void gen(Star* result){
     //Generate Galaxy
-    
     result -> x = rand() % 681;                                     // Generates X position
     result -> y = rand() % 481;                                     // Generates Y position
 }
 
 int main(){
-    srand(time(NULL));
+    srand(time(NULL));                                              // Generate random seed based on time of day
     sf::RenderWindow window(sf::VideoMode(680, 480), "Simulation"); // Define window
     sf::CircleShape shape(3);                                       // Create circle shape
     shape.setFillColor(sf::Color::White);                           // Make it white
@@ -31,17 +30,15 @@ int main(){
                 window.close();
             }
         }
-
     gen(&star);                                                     // Generate galaxy
     shape.setPosition(star.x, star.y);                              // Set shape
     std::map<int, int> points;
     points[star.x]=star.y;                                          //Add to map
     std::cout << star.x, " ", star.y;
     std::cout << "\n";
-    //std::cout << points.first
     window.draw(shape);                                             // Draw shapes
     i++;        
-    if (i > 1000){                                                  // When there are 1000 display
+    if (i > 1000){                                                  // When there are 1000 galaxies display
         window.display();
         break;
     }
