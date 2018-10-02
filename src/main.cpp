e3752efe65c493d0cb99f090931cbe7f36948b05
Sean Theisen <seanjtheisen@gmail.com>
2018-10-02 10:52:18 -0400

    added folders

---
+++ src/main.cpp
@@ -0,0 +1,70 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctime>
+#include <map>
+
+#include "star.h"
+//run g++ -c main.cpp
+//after first compile, run g++ main.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
+
+void gen1(blackh_one* result){                                      //Generate Hole One
+    result -> x = rand() % 681;                                     // Generates X position
+    result -> y = rand() % 481;                                     // Generates Y position
+    result -> size = rand() % 25;                                   // Generate Size
+}
+
+void gen2(blackh_two* result){
+    result -> x = rand() % 681;
+    result -> y = rand() % 481;
+    result -> size = rand() % 25;
+}
+
+
+int main(){
+    srand(time(NULL));                                              // Generate random seed based on time of day
+    sf::RenderWindow window(sf::VideoMode(680, 480), "Simulation"); // Define window
+    sf::CircleShape bho(5);                                         // Create circle shape
+    bho.setFillColor(sf::Color::White);                             // Make it white
+    sf::CircleShape bht(4);
+    bht.setFillColor(sf::Color::White);
+    window.clear();                                                 // Clear the window
+    blackh_one one = {1, 1, 5};                                     // Define struct
+    blackh_two two = {1, 1, 5};
+    int i = 0;                                                      // Define i
+    while (window.isOpen()){                                        // Window event
+        sf::Event event;
+        while (window.pollEvent(event)){
+            if (event.type == sf::Event::Closed){
+                window.close();
+            }
+        }
+    gen1(&one);                                                      // One black hole 
+    bho.setPosition(one.x, one.y);                                   // Set shape
+    std::map<int, int> bh1;
+    bh1[one.x]=one.y;                                                //Add to map
+    std::cout << one.x, " ", one.y;
+    std::cout << "\n";
+    window.draw(bho);                                                // Draw shapes
+    gen2(&two);
+    bht.setPosition(two.x, two.y);
+    std::map<int, int> bh2;
+    bh2[two.x]=two.y;
+    window.draw(bht);
+    window.display();
+    break;
+    }
+    while (window.isOpen()){                                        // Window event
+        sf::Event event;
+        while (window.pollEvent(event)){
+            
+            if (event.type == sf::Event::Closed){
+                window.close();
+            }
+            
+            
+        }
+    }
+    return 0;
+}
---
+++ src/star.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct blackh_one{
+    int x = 0;
+    int y = 0;
+    int size = 0;
+};
+struct blackh_two{
+    int x = 0;
+    int y = 0;
+    int size = 0;
+};
