bdaed862e55ef4517efa6dc10ed5df468994c286
Sean Theisen <seanjtheisen@gmail.com>
2018-10-19 20:47:06 -0400

    Refactored Code

--- src/main.cpp
+++ src/main.cpp
@@ -4,36 +4,16 @@
 #include <unistd.h>
 
 #include "star.h"
+//#include "func.cpp"
 
 #define BIGGPULL 84
 
 unsigned int microseconds;
 
-void gen1(blackh_one* result){                                     //Generate Hole One
-    result -> x = 1 + rand() %((340 + 1) - 1);                     // Generates X position
-    result -> y = 1 + rand() %((481 + 1) - 1);                     // Generates Y position
-    result -> size = 10 + rand() % ((35 + 1) - 10);                // Generate Size
-}
-
-void gen2(blackh_two* result){
-    result -> x = 340 + rand() %((681 + 1) - 340);
-    result -> y = 1 + rand() %((481 + 1) - 1);
-    result -> size = 10 + rand() % ((35 + 1) - 10);
-}
-
-sf::CircleShape intOne(blackh_one * result, sf::CircleShape *bho){                                 
-    bho->setFillColor(sf::Color::White);
-    bho->setPosition(result->x, result->y);
-    bho->setOrigin(bho->getRadius(), bho->getRadius());                                             //Draw object
-    return *bho;
-}
-
-sf::CircleShape intTwo(blackh_two * result, sf::CircleShape *bht){
-    bht->setFillColor(sf::Color::Blue);
-    bht->setOrigin(bht->getRadius(), bht->getRadius());
-    bht->setPosition(result->x, result->y);
-    return *bht;
-}
+void gen1(blackh_one* result);
+void gen2(blackh_two* result);
+sf::CircleShape intOne(blackh_one * result, sf::CircleShape *bho);
+sf::CircleShape intTwo(blackh_two * result, sf::CircleShape *bht);
 
 int main(){
     srand(time(NULL));                                              // Generate random seed based on time of day
