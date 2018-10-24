6d889488bc70c097abf0609858a75adc956c027f
Sean Theisen <seanjtheisen@gmail.com>
2018-10-24 08:36:25 -0400

    Added click to restart functionality

--- src/main.cpp
+++ src/main.cpp
@@ -31,67 +31,68 @@ int main(){
     sf::CircleShape *bho = new sf::CircleShape(one.size);  			// Define bho pointer object
     intOne(&one, bho);						                        // Populate bho pointer object
     intTwo(&two, bht);						                        // Populate bht pointer object
-    window.draw(*bho);						    					// Draw black hole one
-    window.draw(*bht);						    					// Draw black hole two
     int sx = bho->getRadius() + bht->getRadius();		    		// Define sx variable
     sf::CircleShape *bh3 = new sf::CircleShape(sx);					// Define black hole 3 object
     bh3->setPosition(7000, 0);					    				// Set position of black hole 3 object off screen
 	sf::CircleShape *waves = new sf::CircleShape(sx);
+	window.draw(*bho);						    					// Draw black hole one
+    window.draw(*bht);						    					// Draw black hole two
+	window.display();
 //------------------------------------------------------------------------------------
     int spd = 85;  						   // Define speed variable
     while (window.isOpen()){	    		// Window event
-	window.clear();						   // Clear the screen
-	window.draw(*bh3);					  // Draw black hole 3
-	window.display();					  // Display shapes on screen
+		window.clear();						   // Clear the screen
+		window.draw(*bh3);					  // Draw black hole 3
+		window.display();					  // Display shapes on screen
 //------------------------------------------------------------------------------------
-	if (bho->getRadius() > bht->getRadius()){
-		spd -= 2;					    // Decrement speed vaiable
-		bho->move(-1 * (bho->getPosition().x - bht->getPosition().x) / BIGGPULL ,-1 * (bho->getPosition().y - bht->getPosition().y) / BIGGPULL );	// Move the larger black hole towards the smaller black hole
-		bht->move((bho->getPosition().x - bht->getPosition().x) / spd,(bho->getPosition().y - bht->getPosition().y) / spd );						// Move the smaller black hole towards the larger black hole 
-		window.draw(*bho);
-		window.draw(*bht);
-		window.display();
-		usleep(100000);
+		if (bho->getRadius() > bht->getRadius()){
+			spd -= 2;					    // Decrement speed vaiable
+			bho->move(-1 * (bho->getPosition().x - bht->getPosition().x) / BIGGPULL ,-1 * (bho->getPosition().y - bht->getPosition().y) / BIGGPULL );	// Move the larger black hole towards the smaller black hole
+			bht->move((bho->getPosition().x - bht->getPosition().x) / spd,(bho->getPosition().y - bht->getPosition().y) / spd );						// Move the smaller black hole towards the larger black hole 
+			window.draw(*bho);
+			window.draw(*bht);
+			window.display();
+			usleep(100000);
 //--------------------------------------------------------------------------------------
-	}else{
-		spd -= 2;
-    	    	bht->move((bho->getPosition().x - bht->getPosition().x) / BIGGPULL ,(bho->getPosition().y - bht->getPosition().y) / BIGGPULL );
-    	    	bho->move(-1 * (bho->getPosition().x - bht->getPosition().x) / spd,-1 * (bho->getPosition().y - bht->getPosition().y) / spd );
-    	    	window.draw(*bho);
-    	    	window.draw(*bht);
-    	    	window.display();
-		usleep(100000);
-	}
+		}else{
+			spd -= 2;
+    		    	bht->move((bho->getPosition().x - bht->getPosition().x) / BIGGPULL ,(bho->getPosition().y - bht->getPosition().y) / BIGGPULL );
+    		    	bho->move(-1 * (bho->getPosition().x - bht->getPosition().x) / spd,-1 * (bho->getPosition().y - bht->getPosition().y) / spd );
+    		    	window.draw(*bho);
+    		    	window.draw(*bht);
+    		    	window.display();
+			usleep(100000);
+		}
 //-------------------------------------------------------------------------------------
-	if (bho->getPosition() == bht->getPosition()){
-		intThree(bh3, bht, bho);
-		window.clear();
-		window.draw(*bh3);
-		window.display();
-		intWaves(waves, bh3);
-		for(int i = 0; i < 100; i++){
-			growWaves(waves, bh3);
+		if (bho->getPosition() == bht->getPosition()){
+			intThree(bh3, bht, bho);
 			window.clear();
-			window.draw(*waves);
 			window.draw(*bh3);
 			window.display();
-			usleep(10000);
+			intWaves(waves, bh3);
+			
+			for(int i = 0; i < 100; i++){
+				growWaves(waves, bh3);
+				window.clear();
+				window.draw(*waves);
+				window.draw(*bh3);
+				window.display();
+				usleep(10000);
+			}
 		}
-		/*This will be used for a reset button
-						window.close();
-						delete bho, bht, bh3, waves;
-						main();
-					*/
-	}
 //-------------------------------------------------------------------------------------
-	sf::Event event;
-	while (window.pollEvent(event)){
-		if (event.type == sf::Event::Closed){
-			delete bho, bht, bh3, waves;
-			window.close();
+		sf::Event event;
+		while (window.pollEvent(event)){
+			if (event.type == sf::Event::Closed){
+				delete bho, bht, bh3, waves;
+				window.close();
+			}else if (event.mouseButton.button == sf::Mouse::Left){
+				window.close();
+				delete bho, bht, bh3, waves;
+				main();
+			}
 		}
-	}
-	}
+    }
 //------------------------------------------------------------------------------------
     return 0;
 }
